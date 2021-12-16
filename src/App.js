15c8ac7eed37e8ba5fac2db38aca23ec2f9ea750
Jovan McLeod <jovan.ml2001@gmail.com>
2021-12-16 18:31:40 -0500

    removed redundant get request

--- src/App.js
+++ src/App.js
@@ -5,23 +5,18 @@ function App() {
 
   const [output, setOutput] = useState("");
 
-
   const sendText = async (text) =>{
-    await fetch('https://google-translate-app-1221.herokuapp.com/',{
+    const res = await fetch('https://google-translate-app-1221.herokuapp.com/',{
       method: 'POST',
       headers: {
         'Content-type': 'application/json'
       },
       body: JSON.stringify({ text: `${text}` })
     });
-    const translation = await getTranslation();
-    setOutput(translation);
-  };
 
-  const getTranslation = async () =>{
-    const res = await fetch('https://google-translate-app-1221.herokuapp.com/');
-    const data = await res.json()
-    return data.text;
+    const data = await res.json();
+    const translation = await data.text
+    await setOutput(translation);
   };
 
   return (
